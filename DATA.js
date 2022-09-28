23c0f0ee1d40d2990c78762f7cebbee28cc7f3d4
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-29 02:20:53 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "89364a61-4ff4-42d7-918c-1e01a4e5156f",
+    "home_feed": [
+        {
+            "id": "d153278f-6e67-4ed9-97cd-2a6443aa80b4",
+            "artist_name": "note",
+            "home_carousel": [
+                {
+                    "id": "e3c686b6-53fd-4e40-9183-fcbe273b2db4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/351.png"
+                },
+                {
+                    "id": "2bed0753-a358-48ff-be81-afe382ce2d78",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/225.png"
+                },
+                {
+                    "id": "5cb830c3-a6dc-420d-9afb-1ce06e5567a2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/264.png"
+                },
+                {
+                    "id": "38e4c395-c778-40d6-a20e-c500bd44d44f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/116.png"
+                },
+                {
+                    "id": "73bdca67-d092-4d81-b3a5-8be2c961b1f2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/10.png"
+                }
+            ],
+            "art_caption": "general",
+            "art_description": "Realize less third part. Likely space hour the involve seem."
+        },
+        {
+            "id": "89ff0695-d4ec-4446-a71a-7ec11a7ac933",
+            "artist_name": "almost",
+            "home_carousel": [
+                {
+                    "id": "7fa96229-f69f-49c2-b10c-17d2723491a8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/199.png"
+                },
+                {
+                    "id": "508535fa-ca26-4b5a-bce7-3968d55aecdc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/263.png"
+                },
+                {
+                    "id": "1060d3b5-fb4a-4d72-a5f8-5fd05831fe37",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/320.png"
+                },
+                {
+                    "id": "017ede29-af04-423a-9055-70ec86133488",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/22.png"
+                },
+                {
+                    "id": "bcef8f6f-240b-432e-96d1-188e5745e7fe",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/144.png"
+                }
+            ],
+            "art_caption": "analysis",
+            "art_description": "Bar long fire show break. North tend beyond the. Adult wear wear he."
+        },
+        {
+            "id": "4f35e1ed-382b-4f76-ab84-2bebce2e5bf1",
+            "artist_name": "religious",
+            "home_carousel": [
+                {
+                    "id": "beb0b1a4-b061-404e-aa3c-3fff25ec885a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/256.png"
+                },
+                {
+                    "id": "6317caf7-7b7d-4b85-8be0-950b81cda492",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/51.png"
+                },
+                {
+                    "id": "fbfc14c7-2763-4b12-ac8f-b860fb31ee20",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/88.png"
+                },
+                {
+                    "id": "8a2c7bdc-3f81-449b-9edc-9830ca5442cd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/281.png"
+                },
+                {
+                    "id": "0c25402f-86e1-4700-9627-77b4773e4fcd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/311.png"
+                }
+            ],
+            "art_caption": "cause",
+            "art_description": "Maybe may billion avoid attention once someone message. Position issue more although clear add. Health medical cover staff reduce kind."
+        },
+        {
+            "id": "f60b2bb2-5a9a-414f-87af-36716c0514fd",
+            "artist_name": "much",
+            "home_carousel": [
+                {
+                    "id": "d9e2e29a-7d46-4d2b-a6b5-ecf027a07de8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/79.png"
+                },
+                {
+                    "id": "8af57441-a07a-4c77-909f-369c7783771e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/345.png"
+                },
+                {
+                    "id": "44f67e3f-9cdb-451e-b77a-bf98844b2a64",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/206.png"
+                },
+                {
+                    "id": "d901da89-219f-4b15-a69e-d77f60169066",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/99.png"
+                },
+                {
+                    "id": "ca559d4d-342d-4f61-b039-b8e995efe352",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/15.png"
+                }
+            ],
+            "art_caption": "human",
+            "art_description": "Notice decade TV agent late standard table. Away statement toward model good."
+        },
+        {
+            "id": "5f320311-840b-4aad-aae0-f9beeaee9997",
+            "artist_name": "talk",
+            "home_carousel": [
+                {
+                    "id": "2ecd83f6-56dc-4826-aa81-f22e5b4bd6c9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/232.png"
+                },
+                {
+                    "id": "860eeb6f-1fd1-47eb-879e-ca5f72b51ed3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/296.png"
+                },
+                {
+                    "id": "281bc6d5-f1ca-4e03-b625-2554fe12ca0a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/125.png"
+                },
+                {
+                    "id": "c05ea0a1-1ec6-467a-bd76-251773ca749d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/267.png"
+                },
+                {
+                    "id": "3ac88a80-b004-4b1e-91b7-42fc32a69887",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/158.png"
+                }
+            ],
+            "art_caption": "response",
+            "art_description": "Attention body TV green prevent. Successful represent shoulder table hour up usually. Foot result there watch."
+        }
+    ]
+}
